130b7ec4fbb32613b6f941d37d45a3f2b495daa2
Aron Vinicius <aronvinicius15@gmail.com>
2022-11-03 23:30:22 -0300

    add websocket client with signalr

---
+++ signalr-websocket-client/signalr-websocket-client/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System.Threading.Tasks;
+
+namespace signalr_websocket_client
+{
+    public class Program
+    {
+        static async Task Main()
+        {
+            var uri = "http://localhost:2100/websocket";
+
+            await using var connection = new HubConnectionBuilder().WithUrl(uri).Build();
+            await connection.StartAsync();
+
+            connection.On<dynamic>("SendValues", (message) => {
+                System.Console.WriteLine(message); 
+            });
+
+            System.Console.ReadLine();
+        }
+    }
+}
